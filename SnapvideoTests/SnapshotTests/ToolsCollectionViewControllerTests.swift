1b6598d38b580fef1fa97db986d270f59b58c6d7
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-06 12:36:55 +0000

    add snapshot test for ToolsCollectionViewController

---
+++ SnapvideoTests/SnapshotTests/ToolsCollectionViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  VideoEditorViewControllerTests.swift
+//  SnapvideoTests
+//
+//  Created by Anastasia Petrova on 06/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Snapvideo
+
+class ToolsCollectionViewControllerTests: XCTestCase {
+
+    func testHomeViewController() {
+      let vc = ToolsCollectionViewController()
+
+      assertSnapshot(matching: vc, as: .image)
+    }
+}
