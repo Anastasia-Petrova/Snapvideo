7fb2be4d026dba88175dea1cedc03b2542fb9504
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-06 12:37:50 +0000

    add snapshot tests for HomeViewController

---
+++ SnapvideoTests/SnapshotTests/HomeViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewControllerTests.swift
+//  SnapvideoTests
+//
+//  Created by Anastasia Petrova on 06/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Snapvideo
+
+class HomeViewControllerTests: XCTestCase {
+
+    func testHomeViewController() {
+      let vc = HomeViewController()
+
+      assertSnapshot(matching: vc, as: .image)
+    }
+}
