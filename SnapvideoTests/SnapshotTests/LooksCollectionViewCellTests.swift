ddf9a88aa587bc4e267839f7e989cac28c5229ec
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-07 18:25:00 +0000

    update snapshot tests re-record snapshots

--- SnapvideoTests/SnapshotTests/LooksCollectionViewCellTests.swift
+++ SnapvideoTests/SnapshotTests/LooksCollectionViewCellTests.swift
@@ -13,19 +13,13 @@ import SnapshotTesting
 class LooksCollectionViewCellTests: XCTestCase {
     func testLooksCollectionViewCell_image_has_Placeholder() {
         let cell = LooksCollectionViewCell(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 60, height: 76)))
-        cell.filterName.text = "BlurFilter"
+        cell.filterName.text = "Blur"
         assertSnapshot(matching: cell, as: .image)
     }
     
     func testLooksCollectionViewCell_long_name() {
         let cell = LooksCollectionViewCell(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 60, height: 76)))
-        cell.filterName.text = "BlurFilterBlurFilterBlurFilterBlurFilterBlurFilterBlurFilterBlurFilterBlurFilterBlurFilterBlurFilter"
-        assertSnapshot(matching: cell, as: .image)
-    }
-    
-    func testLooksCollectionViewCell_image() {
-        let cell = LooksCollectionViewCell(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 60, height: 76)))
-        cell.filterName.text = "BlurFilter"
+        cell.filterName.text = "Very very very very long title"
         assertSnapshot(matching: cell, as: .image)
     }
 }
