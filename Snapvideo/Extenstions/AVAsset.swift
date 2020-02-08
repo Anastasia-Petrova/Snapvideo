84298c318e11809fd063abdd234fb40022eebb2c
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-08 21:14:24 +0000

    add Copyrights

--- Snapvideo/Extenstions/AVAsset.swift
+++ Snapvideo/Extenstions/AVAsset.swift
@@ -1,3 +1,11 @@
+//
+//  AVAsset.swift
+//  Snapvideo
+//
+//  Created by Anastasia Petrova on 02/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
 import AVFoundation
 import UIKit
 
--- Snapvideo/Filters/Filter.swift
+++ Snapvideo/Filters/Filter.swift
@@ -1,3 +1,11 @@
+//
+//  Filter.swift
+//  Snapvideo
+//
+//  Created by Anastasia Petrova on 02/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
 import Foundation
 import CoreImage
 
