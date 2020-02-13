3df2e75b3d7963127ac83d634be9606beb3bdf86
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-13 17:23:23 +0000

    create EditingWithToolViewController

---
+++ Snapvideo/EditingWithToolViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ToolEditingViewController.swift
+//  Snapvideo
+//
+//  Created by Anastasia Petrova on 13/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class EditingWithToolViewController: UIViewController {
+    let asset: AVAsset
+    let player: AVPlayer
+    let playerView: VideoView
+    var playerRateObservation: NSKeyValueObservation?
+    let tabBar = TabBar(items: "✕", "✓")
+    lazy var resumeImageView = UIImageView(image: UIImage(named: "playCircle")?.withRenderingMode(.alwaysTemplate))
+    var slider = UISlider()
+    
+    var trackDuration: Float {
+        guard let trackDuration = player.currentItem?.asset.duration else {
+            return 0
+        }
+        return Float(CMTimeGetSeconds(trackDuration))
+    }
+    
+    init(url: URL, tool: AnyTool) {
+        asset = AVAsset(url: url)
+        let playerItem = AVPlayerItem(asset: asset)
+        player = AVPlayer(playerItem: playerItem)
+        let output = AVPlayerItemVideoOutput(outputSettings: nil)
+        playerItem.add(output)
+        playerView = VideoView(
+            videoOutput: output,
+            videoOrientation: self.asset.videoOrientation
+        )
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        view.addSubview(playerView)
+        view.addSubview(tabBar)
+        setUpPlayerView()
+        setUpResumeButton()
+        setUpPlayer()
+        setUpSlider()
+        setUpTabBar()
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+         if player.rate == 0 {
+             player.play()
+         } else {
+             player.pause()
+         }
+     }
+     
+     deinit {
+         NotificationCenter.default.removeObserver(self)
+     }
+     
+     private func setUpTabBar() {
+         tabBar.delegate = self
+         tabBar.translatesAutoresizingMaskIntoConstraints = false
+         tabBar.setContentHuggingPriority(.required, for: .vertical)
+         tabBar.setContentCompressionResistancePriority(.required, for: .vertical)
+         NSLayoutConstraint.activate([
+             tabBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+             tabBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+             tabBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+         ])
+     }
+    
+    func setUpPlayer() {
+        //1 Подписка на событие достижения конца видео
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(playerItemDidReachEnd(notification:)),
+            name: .AVPlayerItemDidPlayToEndTime,
+            object: player.currentItem
+        )
+        
+        //2 Подписка на изменение рейта плейера - (играю/не играю)
+        playerRateObservation = player.observe(\.rate) { [weak self] (_, _) in
+            guard let self = self else { return }
+            let isPlaying = self.player.rate > 0
+            self.resumeImageView.isHidden = isPlaying
+            isPlaying ? self.playerView.play() :  self.playerView.pause()
+        }
+        
+        player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 100), queue: .main) { [weak self] time in
+            self?.slider.value = Float(time.seconds)
+        }
+    }
+    
+    func setUpResumeButton() {
+        resumeImageView.translatesAutoresizingMaskIntoConstraints = false
+        playerView.addSubview(resumeImageView)
+        NSLayoutConstraint.activate([
+            resumeImageView.centerYAnchor.constraint(equalTo: playerView.centerYAnchor),
+            resumeImageView.centerXAnchor.constraint(equalTo: playerView.centerXAnchor),
+            resumeImageView.heightAnchor.constraint(equalToConstant: 70),
+            resumeImageView.widthAnchor.constraint(equalToConstant: 70)
+        ])
+        resumeImageView.tintColor = .white
+        resumeImageView.isUserInteractionEnabled = false
+        resumeImageView.isHidden = false
+    }
+    
+    func setUpSlider() {
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        playerView.addSubview(slider)
+        NSLayoutConstraint.activate ([
+            slider.leadingAnchor.constraint(equalTo: playerView.leadingAnchor, constant: 50),
+            slider.trailingAnchor.constraint(equalTo: playerView.trailingAnchor, constant: -50),
+            slider.bottomAnchor.constraint(equalTo: playerView.bottomAnchor, constant: -40)])
+        slider.minimumValue = 0
+        slider.maximumValue = trackDuration
+        slider.addTarget(self, action: #selector(self.sliderAction), for: .valueChanged)
+    }
+    
+    func setUpPlayerView() {
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate ([
+            playerView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            playerView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            playerView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tabBar.topAnchor.constraint(greaterThanOrEqualTo: playerView.bottomAnchor)
+        ])
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        playerView.addGestureRecognizer(tap)
+    }
+    
+    @objc func playerItemDidReachEnd(notification: Notification) {
+        if let playerItem = notification.object as? AVPlayerItem {
+            playerItem.seek(to: CMTime.zero, completionHandler: nil)
+        }
+    }
+    
+    @objc func sliderAction() {
+        player.seek(
+            to: CMTime(
+                seconds: Double(slider.value),
+                preferredTimescale: 1
+            ),
+            toleranceBefore: CMTime.zero,
+            toleranceAfter: CMTime.zero
+        )
+    }
+    
+}
+
+extension EditingWithToolViewController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+
+    }
+}
