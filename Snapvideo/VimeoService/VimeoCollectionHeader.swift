f7b16c414a33eadb959d52c79f3ea3916de881a6
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-20 17:13:08 +0100

    improve vimeoCollectionHeader layout

--- Snapvideo/VimeoService/VimeoCollectionHeader.swift
+++ Snapvideo/VimeoService/VimeoCollectionHeader.swift
@@ -8,29 +8,63 @@
 
 import UIKit
 
-class VimeoCollectionHeader: UICollectionViewCell {
+final class VimeoCollectionHeader: UICollectionViewCell {
     static let identifier = "VimeoCollectionHeader"
     
-    let uploadVideoButton = UIButton()
+    let imageView = ExportImageView(systemName: "arrow.up.right.video")
+    let uploadVideoButton = HightlightedButton()
     
     override init(frame: CGRect) {
     super.init(frame: frame)
-        uploadVideoButton.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(uploadVideoButton)
-        NSLayoutConstraint.activate([
-            uploadVideoButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            uploadVideoButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            uploadVideoButton.topAnchor.constraint(equalTo: contentView.topAnchor),
-            uploadVideoButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-        ])
-        uploadVideoButton.setTitle("Upload video", for: .normal)
-        uploadVideoButton.setTitleColor(.lightGray, for: .normal)
-        uploadVideoButton.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
-        uploadVideoButton.addTarget(self, action: #selector(handleUpload), for: .touchUpInside)
+        setUpHeaderView()
+        setUpUploadButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpHeaderView() {
+        let stackView = UIStackView()
+        let image = UIImage(systemName: "arrow.up.right.video")?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .systemBlue
+        
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .medium)
+        label.numberOfLines = 1
+        label.textColor = .systemBlue
+        label.textAlignment = .left
+        label.text = "Upload video"
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 30
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 35),
+            imageView.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+    private func setUpUploadButton() {
+        uploadVideoButton.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(uploadVideoButton)
+        NSLayoutConstraint.activate ([
+            uploadVideoButton.leadingAnchor.constraint(equalTo:  contentView.leadingAnchor),
+            uploadVideoButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            uploadVideoButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            uploadVideoButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        uploadVideoButton.addTarget(self, action: #selector(handleUpload), for: .touchUpInside)
     }
     
     @objc func handleUpload() {
