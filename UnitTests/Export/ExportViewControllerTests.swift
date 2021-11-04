d625c97a757e3ec66bff79b3a93e4f1976ddfcf8
Anastasia Petrova <anastasia.iosdev@gmail.com>
2021-11-04 19:38:58 +0000

    add ExportViewControllerTests

---
+++ UnitTests/Export/ExportViewControllerTests.swift
@@ -0,0 +1,46 @@
+//
+//  ExportViewControllerTests.swift
+//  UnitTests
+//
+//  Created by Anastasia Petrova on 03/11/2021.
+//  Copyright © 2021 Anastasia Petrova. All rights reserved.
+//
+
+import XCTest
+@testable import Snapvideo
+
+class ExportViewControllerTests: XCTestCase {
+  var exportVC: ExportViewController!
+  
+  override func setUpWithError() throws {
+    exportVC = ExportViewController()
+  }
+  
+  override func tearDownWithError() throws {
+    exportVC = nil
+  }
+  
+  func test_saveVideoCopy() {
+    var capturedActions: [ExportAction] = []
+    exportVC.didTapEportViewButton = { action in
+      capturedActions.append(action)
+    }
+    
+    exportVC.loadViewIfNeeded()
+    exportVC.saveCopyButton.sendActions(for: .touchUpInside)
+    
+    XCTAssertEqual(capturedActions, [ExportAction.saveVideoCopy])
+  }
+  
+  func test_openActivityView() {
+    var capturedActions: [ExportAction] = []
+    exportVC.didTapEportViewButton = { action in
+      capturedActions.append(action)
+    }
+    
+    exportVC.loadViewIfNeeded()
+    exportVC.shareButton.sendActions(for: .touchUpInside)
+    
+    XCTAssertEqual(capturedActions, [ExportAction.openActivityView])
+  }
+}
