3f301e3c8bf5d981922c54314d50cd23af8d4732
Anastasia Petrova <anastasia.iosdev@gmail.com>
2021-11-16 07:00:59 +0000

    add snapshot tests for SpeedViewController

---
+++ SnapshotTests/Adjustments/SpeedViewControllerSnapshotTests.swift
@@ -0,0 +1,25 @@
+//
+//  SpeedViewControllerSnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by Anastasia Petrova on 16/11/2021.
+//  Copyright © 2021 Anastasia Petrova. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Snapvideo
+
+final class SpeedViewControllerSnapshotTests: XCTestCase {
+  func testSpeedViewController() {
+    guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
+        XCTFail("testVideo.MOV not found")
+        return
+    }
+    let url = URL(fileURLWithPath: path)
+    let vc = SpeedViewController(url: url) { _ in }
+    vc.view.layer.borderColor = UIColor.red.cgColor
+    vc.view.layer.borderWidth = 4
+    assertSnapshot(matching: vc, as: .image)
+  }
+}
