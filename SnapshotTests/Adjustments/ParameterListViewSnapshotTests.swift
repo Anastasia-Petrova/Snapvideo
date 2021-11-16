1fe993e1ae1260b190a09f8f95738c343a093951
Anastasia Petrova <anastasia.iosdev@gmail.com>
2021-11-16 19:57:28 +0000

    clean up AdjustViewControllerSnapshotTests

--- SnapshotTests/Adjustments/ParameterListViewSnapshotTests.swift
+++ SnapshotTests/Adjustments/ParameterListViewSnapshotTests.swift
@@ -11,40 +11,27 @@ import SnapshotTesting
 @testable import Snapvideo
 
 final class ParameterListViewSnapshotTests: XCTestCase {
-    func test_single_row() {
-        let vc = UIViewController()
-        let view = ParameterListView(parameters: [
-            Parameter(name: "Hello", value: 10, minPercent: 0)
-        ])
-        add(view, on: vc)
-        view.backgroundColor = .red
-        assertSnapshot(matching: vc, as: .image)
-    }
+  func test_single_row() {
+    let vc = UIViewController()
+    let view = ParameterListView(parameters: [
+      Parameter(name: "Hello", value: 10, minPercent: 0)
+    ])
+    vc.add(view)
+    view.backgroundColor = .red
+    assertSnapshot(matching: vc, as: .image)
+  }
+  
+  func test_multiple_row() {
+    let vc = UIViewController()
+    let view = ParameterListView(parameters: [
+      Parameter(name: "Hello", value: 10, minPercent: 0),
+      Parameter(name: "World", value: 0, minPercent: 0),
+      Parameter(name: "Foo", value: 3, minPercent: 0),
+      Parameter(name: "Bar", value: -23, minPercent: 0),
+    ])
+    vc.add(view)
+    view.backgroundColor = .red
     
-    func test_multiple_row() {
-        let vc = UIViewController()
-        let view = ParameterListView(parameters: [
-            Parameter(name: "Hello", value: 10, minPercent: 0),
-            Parameter(name: "World", value: 0, minPercent: 0),
-            Parameter(name: "Foo", value: 3, minPercent: 0),
-            Parameter(name: "Bar", value: -23, minPercent: 0),
-        ])
-        add(view, on: vc)
-        view.backgroundColor = .red
-        
-        assertSnapshot(matching: vc, as: .image)
-    }
-    
-    func add(_ view: UIView, on vc: UIViewController) {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        vc.view.backgroundColor = .black
-        vc.view.addSubview(view)
-        NSLayoutConstraint.activate([
-            view.widthAnchor.constraint(equalTo: vc.view.widthAnchor, multiplier: 0.7),
-            view.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor),
-            view.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor),
-        ])
-        vc.viewDidLoad()
-        vc.view.layoutIfNeeded()
-    }
+    assertSnapshot(matching: vc, as: .image)
+  }
 }
