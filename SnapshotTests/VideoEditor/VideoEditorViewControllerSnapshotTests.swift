58bd27d959d84d24be28f7c8b95a6787654ac242
Anastasia Petrova <anastasia.iosdev@gmail.com>
2021-11-16 10:26:39 +0000

    update VideoEditorViewController

--- SnapshotTests/VideoEditor/VideoEditorViewControllerSnapshotTests.swift
+++ SnapshotTests/VideoEditor/VideoEditorViewControllerSnapshotTests.swift
@@ -11,67 +11,58 @@ import SnapshotTesting
 @testable import Snapvideo
 
 final class VideoEditorViewControllerSnapshotTests: XCTestCase {
-    func DISABLED_testVideoEditorViewController() {
-        let filters = [PassthroughFilter()]
-        let tools: Array<ToolEnum> = [.vignette(tool: VignetteTool())]
-        guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
-            XCTFail("testVideo.MOV not found")
-            return
-        }
-        let url = URL(fileURLWithPath: path)
-        let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
-        
-        assertSnapshot(matching: vc, as: .wait(for: 2, on: .image))
+  var filters: [Filter]!
+  var tools: Array<ToolEnum>!
+  var url: URL!
+  
+  override func setUp() {
+    filters = [PassthroughFilter()]
+    tools = [.vignette(tool: VignetteTool())]
+    guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
+        XCTFail("testVideo.MOV not found")
+        return
     }
+    url = URL(fileURLWithPath: path)
+  }
+  
+  override func tearDown() {
+    filters = nil
+    tools = nil
+    url = nil
+  }
+  
+  func testVideoEditorViewController() {
+    let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
+    assertSnapshot(matching: vc, as: .wait(for: 2, on: .image))
+  }
+  
+  func testVideoEditorViewController_openLooks_and_closeLooks_set_correct_constraints() {
+    let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
     
-    func DISABLED_testVideoEditorViewController_openLooks_and_closeLooks_set_correct_constraints() {
-        let filters = [PassthroughFilter()]
-        let tools: Array<ToolEnum> = [.vignette(tool: VignetteTool())]
-        guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
-            XCTFail("testVideo.MOV not found")
-            return
-        }
-        let url = URL(fileURLWithPath: path)
-        let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
-        
-        vc.openLooks()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-        
-        vc.closeLooks()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-    }
+    vc.openLooks()
+    assertSnapshot(matching: vc, as: .wait(for: 3, on: .image))
     
-    func DISABLED_testVideoEditorViewController_openTools_and_closeTools_set_correct_constraints() {
-        let filters = [PassthroughFilter()]
-        let tools: Array<ToolEnum> = [.vignette(tool: VignetteTool())]
-        guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
-            XCTFail("testVideo.MOV not found")
-            return
-        }
-        let url = URL(fileURLWithPath: path)
-        let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
-
-        vc.openTools()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-        
-        vc.closeTools()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-    }
+    vc.closeLooks()
+    assertSnapshot(matching: vc, as: .wait(for: 3, on: .image))
+  }
+  
+  func testVideoEditorViewController_openTools_and_closeTools_set_correct_constraints() {
+    let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
     
-    func DISABLED_testVideoEditorViewController_openExportMenu_and_closeExportMenu_set_correct_constraints() {
-        let filters = [PassthroughFilter()]
-        let tools: Array<ToolEnum> = [.vignette(tool: VignetteTool())]
-        guard let path = Bundle.snapshotTests.path(forResource: "videoTest", ofType:"MOV") else {
-            XCTFail("testVideo.MOV not found")
-            return
-        }
-        let url = URL(fileURLWithPath: path)
-        let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
-        
-        vc.openExportMenu()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-        
-        vc.closeExportMenu()
-        assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
-    }
+    vc.openTools()
+    assertSnapshot(matching: vc, as: .wait(for: 3, on: .image))
+    
+    vc.closeTools()
+    assertSnapshot(matching: vc, as: .wait(for: 3, on: .image))
+  }
+  
+  func testVideoEditorViewController_openExportMenu_and_closeExportMenu_set_correct_constraints() {
+    let vc = VideoEditorViewController(url: url, selectedFilterIndex: 0, filters: filters, tools: tools)
+    
+    vc.openExportMenu()
+    assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
+    
+    vc.closeExportMenu()
+    assertSnapshot(matching: vc, as: .wait(for: 5, on: .image))
+  }
 }
