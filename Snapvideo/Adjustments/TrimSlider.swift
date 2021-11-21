0a91699dd97c9d4b42f2b1d8c39b670d24c2142c
Anastasia Petrova <anastasia.iosdev@gmail.com>
2021-11-21 17:53:33 +0000

    add trim slider

---
+++ Snapvideo/Adjustments/TrimSlider.swift
@@ -0,0 +1,13 @@
+//
+//  TrimSlider.swift
+//  Snapvideo
+//
+//  Created by Anastasia Petrova on 19/11/2021.
+//  Copyright © 2021 Anastasia Petrova. All rights reserved.
+//
+
+import Foundation
+
+final class TrimSlider {
+  
+}
